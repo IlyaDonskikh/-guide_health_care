f23ea45830f7b61f987511d543a4d47a4a1caf49
Alexandr Kirshin <akirshin.dev@gmail.com>
2016-10-26 14:53:17 +0400

    update seeds

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,10 +4,10 @@ class Organization < ApplicationRecord
   ## Relations
   has_many :organization_illnesses, dependent: :destroy
   has_many :illnesses, through: :organization_illnesses
-  has_many :organization_ages, dependent: :destroy
+  has_many :ages, dependent: :destroy, class_name: 'OrganizationAge'
 
   accepts_nested_attributes_for :organization_illnesses, allow_destroy: true
-  accepts_nested_attributes_for :organization_ages, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :ages, reject_if: :all_blank, allow_destroy: true
 
   ## Scopes
   scope :filter_address, -> (address) { near(address) }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-organizations = JSON.parse(File.read(Rails.root + 'test/fixtures/files/organizations.json'))
+organizations = JSON.parse(File.read(Rails.root + 'db/import_data/organizations.json'))
 organizations.each_with_index do |organization, index|
   Organization.create(name: organization['name'], address: organization['address'])
   sleep 1 if (index + 1) % 5 == 0
 end
-illnesses = JSON.parse(File.read(Rails.root + 'test/fixtures/files/illnesses.json'))
+illnesses = JSON.parse(File.read(Rails.root + 'db/import_data/illnesses.json'))
 illnesses.each do |illness|
   Illness.create(name: illness['name'])
 end
@@ -14,4 +14,11 @@ Illness.all.find_each do |illness|
     illness.organizations << Organization.offset(offset).first
   end
   illness.save
+end
+
+Organization.all.find_each do |organization|
+  2.times do
+    min = rand(0..100)
+    organization.ages.create(min: min, max: rand(min..100))
+  end
 end
